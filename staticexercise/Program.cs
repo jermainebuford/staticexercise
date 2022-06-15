f97035f44eb9bb95f66cf76d7341a1f252490921
jermaine buford <bufordjermaine@outlook.com>
2022-06-15 11:35:45 -0400

    first commit

---
+++ staticexercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace staticexercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Celsius = TempConverter.FahrenheitToCelsius(68);
+
+            var Fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"after conversion");
+
+            Console.WriteLine($"fahrenheit {Fahrenheit}");
+
+            Console.WriteLine($"Celsius {Celsius}");
+
+        }
+    }
+}
